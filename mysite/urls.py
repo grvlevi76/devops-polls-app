8c823d0f42a36ea87517bfb6967fefb0d67269f5
gaurav yadav <grvyadav76@gmail.com>
2026-02-02 19:58:07 +0530

    root not found error fix

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path  #edited by goofy
-
+from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path("polls/", include("polls.urls")), # if user's request url(polls/) matches the 'route' then request get forwarded to specific code 'view' or another URLconf
+    path('', RedirectView.as_view(url='polls/', permanent=True)),
+    path("polls/", include("polls.urls")),
     path('admin/', admin.site.urls),
 ]
